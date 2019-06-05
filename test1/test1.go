f23a1800caebf84834c1a031ef5446611ac25c00
roger <srogerf@gmail.com>
2019-06-04 20:49:15 -0700

    go testing

--- test1/test1.go
+++ test1/test1.go
@@ -1,15 +1,23 @@
 package main
 
 import (
-	"fmt"
+	_ "fmt"
+	_ "github.com/goinaction/code/chapter2/sample/matchers"
+	"github.com/goinaction/code/chapter2/sample/search"
+	"log"
 	_ "log"
+	"os"
 	_ "os"
 )
 
 func init() {
-	//	log.SetOutput(os.Stdout)
-	//	log.Fatal("cc")
+	log.SetOutput(os.Stdout)
+	log.Fatal("init")
 }
 func main() {
-	fmt.Printf("helloi\n\n")
+	log.SetOutput(os.Stdout)
+	log.Fatal("helloi\n\n")
+	i := 5
+	log.Fatal("%s\n\n", i)
+	search.Run("rest")
 }
